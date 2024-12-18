c1aff062109c952f30689b796934dd688e3d3623
Stephen Rosen <sirosen@globus.org>
2024-12-18 14:23:27 -0600

    Add support for code-block detection

---
+++ examples/good_code_block.rst
@@ -0,0 +1,13 @@
+Foo-bar: ``baz``
+
+.. code-block:: python
+
+    # see `foo`
+    bar = foo()
+
+Also, see nested section:
+
+    .. code-block:: python
+
+        # see `baz`
+        foo = baz(1)
--- src/rstbebe/_checkers.py
+++ src/rstbebe/_checkers.py
@@ -4,6 +4,7 @@ import typing as t
 from ._model import ErrorLine, Position
 
 BAD_BACKTICK_PAT = re.compile(r"(^|\s)`[^`]+`([.,?!\s]|$)", flags=re.MULTILINE)
+CODE_BLOCK_START_PAT = re.compile(r"^\s*\.\.\s+code-block::")
 
 
 class BacktickChecker:
@@ -12,7 +13,19 @@ class BacktickChecker:
         self.contents = contents
 
     def iter_errors(self) -> t.Iterator[ErrorLine]:
+        code_block_detector = _CodeBlockDetector()
+
         for lineno, line in enumerate(self.contents, 1):
+            # consume/skip lines while in a code-block
+            if code_block_detector.in_code_block and not code_block_detector.block_end(
+                line
+            ):
+                continue
+
+            # if we are on the first line of a code-block, start skipping!
+            if code_block_detector.set_start(line):
+                continue
+
             line = line.rstrip("\n")
             yield from self._iter_line_errors(line, lineno)
 
@@ -25,3 +38,39 @@ class BacktickChecker:
                 pos=pos,
                 message="found what appear to be markdown backticks",
             )
+
+
+class _CodeBlockDetector:
+    def __init__(self) -> None:
+        self.in_code_block = False
+        self._indent = 0
+
+    def set_start(self, line: str) -> bool:
+        """
+        Try to start a code-block.
+
+        If the line begins a new code-block, return true. Otherwise, false.
+        """
+        if CODE_BLOCK_START_PAT.match(line):
+            self.in_code_block = True
+            self._indent = _get_indent(line)
+            return True
+        return False
+
+    def block_end(self, line: str) -> bool:
+        """
+        Try to end a code-block.
+
+        If the line ends the current code-block, return true. Otherwise, false.
+        """
+        # skip empty lines
+        if not line.strip():
+            return False
+        if _get_indent(line) <= self._indent:
+            self.in_code_block = False
+            return True
+        return False
+
+
+def _get_indent(line: str) -> int:
+    return len(line) - len(line.lstrip())
